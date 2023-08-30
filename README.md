d2d25f63bc536cabd1af42f625f5ab5f73ea6d70
Luca Di Carlo <luca@di-carlo.fr>
2023-08-30 10:12:21 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 using https://github.com/samdroid-apps/nix-articles/blob/master/04-proper-mkderivation.md
+https://matthewrhone.dev/jekyll-in-nixos
--- flake.nix
+++ flake.nix
@@ -6,11 +6,35 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages.${system}.default = pkgs.stdenv.mkDerivation { 
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec
+      {
+        defaultPackage = pkgs.stdenv.mkDerivation rec {
           name = "impure.systems";
           src = ./.;
+          installPhase = ''
+            # Build the site to the $out directory
+            export JEKYLL_ENV=production
+            ${pkgs.jekyll}/bin/jekyll build --destination $out
+          '';
         };
-      });
+        devShells.default = pkgs.mkShell
+          {
+
+            buildInputs = [
+              pkgs.jekyll
+              pkgs.bundler
+              (pkgs.writeShellScriptBin
+                "serve"
+                ''
+                  echo "Running server: visit http://localhost:8000/example/index.html"
+                  ${pkgs.bundler}/bin/bundler install
+                  ${pkgs.jekyll}/bin/jekyll serve .
+                '')
+            ];
+          };
+      }
+    );
 }
