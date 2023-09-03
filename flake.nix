fe9f9c0ab462e7d6cb35500a5a42c12002c04f8e
Luca Di Carlo <luca@di-carlo.fr>
2023-09-03 15:23:54 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -5,40 +5,40 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        env = pkgs.bundlerEnv
-          {
-            name = "env";
-            ruby = pkgs.ruby;
-            gemfile = ./Gemfile;
-            lockfile = ./Gemfile.lock;
-            gemset = ./gemset.nix;
-          };
-      in
-      rec  {
-        devShells.default =
-          pkgs.mkShell {
-            buildInputs = [ pkgs.bundix ];
-          };
-        packages.${system}.default = pkgs.stdenv.mkDerivation {
-          name = "impure-systems";
-
-          src = self;
-          buildInputs = with pkgs; [
-            env
-          ];
-
-          buildPhase = ''
-            ${env}/bin/jekyll build
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp -r _site $out/_site
-          '';
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      env = pkgs.bundlerEnv
+        {
+          name = "env";
+          ruby = pkgs.ruby;
+          gemfile = ./Gemfile;
+          lockfile = ./Gemfile.lock;
+          gemset = ./gemset.nix;
         };
-      }
+      inherit (pkgs) stdenv lib;
+    in
+    rec  {
+      devShells.default =
+        pkgs.mkShell {
+          buildInputs = [ pkgs.bundix ];
+        };
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "impure-systems";
 
-    );
+        src = self;
+        buildInputs = with pkgs; [
+          env
+        ];
+
+        buildPhase = ''
+          ${env}/bin/jekyll build
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r _site $out/_site
+        '';
+      };
+    };
+
 }
